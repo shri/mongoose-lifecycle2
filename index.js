a06ad99a7dc4367964858ba532e324cd7c187980
Shri Ganeshram <shrig94@gmail.com>
2014-09-05 13:07:46 -0700

    remove unnecessary doc variables

--- index.js
+++ index.js
@@ -28,46 +28,42 @@
 module.exports = exports = function lifecycleEventsPlugin(schema) {
   schema.pre('save', function (next) {
     var model = this.model(this.constructor.modelName);
-    var doc = this;
     model.emit('beforeSave', this);
-    schema.emit('beforeSave', doc);
+    schema.emit('beforeSave', this);
     if (this.isNew) {
       model.emit('beforeInsert', this);
-      schema.emit('beforeInsert', doc);
+      schema.emit('beforeInsert', this);
     }
     else {
       model.emit('beforeUpdate', this);
-      schema.emit('beforeUpdate', doc);
+      schema.emit('beforeUpdate', this);
     }
     this._isNew_internal = this.isNew;
     next();
   });
   schema.post('save', function() {
     var model = this.model(this.constructor.modelName);
-    var doc = this;
     model.emit('afterSave', this);
-    schema.emit('afterSave', doc);
+    schema.emit('afterSave', this);
     if (this._isNew_internal) {
       model.emit('afterInsert', this);
-      schema.emit('afterInsert', doc);
+      schema.emit('afterInsert', this);
     }
     else {
       model.emit('afterUpdate', this);
-      schema.emit('afterUpdate', doc);
+      schema.emit('afterUpdate', this);
     }
     this._isNew_internal = undefined;
   });
   schema.pre('remove', function (next) {
     var model = this.model(this.constructor.modelName);
-    var doc = this;
     model.emit('beforeRemove', this);
-    schema.emit('beforeRemove', doc);
+    schema.emit('beforeRemove', this);
     next();
   });
   schema.post('remove', function() {
     var model = this.model(this.constructor.modelName);
-    var doc = this;
     model.emit('afterRemove', this);
-    schema.emit('afterRemove', doc);
+    schema.emit('afterRemove', this);
   });
 };
